4594327cc7871595a1dd179d185b451eab2bb74d
Huzaifah <huzaifah@merimen.com>
2023-02-08 16:56:42 +0800

    add PostList into Post

--- src/components/PostList.tsx
+++ src/components/PostList.tsx
@@ -35,7 +35,7 @@ export class PostList extends Component<IPostList> {
               ))}
             </List>
           </>
-          : "Loading..."
+          : ""
         }
       </>
     )
--- src/pages/Post.tsx
+++ src/pages/Post.tsx
@@ -1,27 +1,35 @@
 import React, { useState, useEffect } from 'react';
 import axios from "axios";
+import Container from '@mui/material/Container';
+import PostList from '../components/PostList';
 import { useSearchParams } from "react-router-dom";
 
 const Post = () => {
   const [post, setPost] = useState<any>(null);
-  let [searchParams, setSearchParams] = useSearchParams();
-  const postId = searchParams.get("postId") ?? 0;
+  let [searchParams, ] = useSearchParams();
+  let postId = searchParams.get("postId") ?? 0;
 
   useEffect(() => {
-    axios.get('https://jsonplaceholder.typicode.com/posts/')
+    if (postId > 0){
+      axios.get(`https://jsonplaceholder.typicode.com/posts/${postId}`)
       .then((res) => {
         setPost(res.data);
       })
       .catch((err) => {
         console.log(err);
       })
-  }, [])
+    }
+  }, [postId])
 
   return (
     <>
-      <p>Post</p>
-      {postId}
-      {JSON.stringify(post)}
+      <Container maxWidth="sm">
+        <h1>{`Post ${postId}`}</h1>
+        {post ?
+          <PostList posts={[post]}/>
+          : "Loading..."
+        }
+      </Container>
     </>
   )
 }
