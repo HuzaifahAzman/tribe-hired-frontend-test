b3a79f078ba14335acb4d36f95ba7b874e97c89b
Huzaifah <huzaifah@merimen.com>
2023-02-08 14:46:46 +0800

    add main page for post

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,12 @@
-import React, { useState, useEffect } from 'react';
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
-import axios from "axios";
+import MainPage from './components/MainPage';
 
 function App() {
-  const [post, setPost] = useState(null);
-
-  useEffect(() => {
-    axios.get('https://jsonplaceholder.typicode.com/posts').then((res) => {
-      setPost(res.data);
-    })
-  }, [])
-
-  console.log("tracing post", post);
-  
 
   return (
-    <div>
-      {!!post ? JSON.stringify(post) : "Loading"}
+    <div className="container">
+      <MainPage />
     </div>
   );
 }
